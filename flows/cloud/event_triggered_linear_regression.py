1961b4d939fbee48e956da16eeda4570ad15f738
Fredrik Olsson <fredrik.olsson@gmail.com>
2023-05-21 11:00:23 +0000

    Project week 4

--- flows/cloud/event_triggered_linear_regression.py
+++ flows/cloud/event_triggered_linear_regression.py
@@ -1,10 +1,13 @@
+
+# Let's use a modified version of the event_triggered_linear_regression.py file from week 3 as the champion.
+
 from metaflow import FlowSpec, step, card, conda_base, current, Parameter, Flow, trigger, project
 from metaflow.cards import Markdown, Table, Image, Artifact
 
 URL = "https://outerbounds-datasets.s3.us-west-2.amazonaws.com/taxi/latest.parquet"
 DATETIME_FORMAT = '%Y-%m-%d %H:%M:%S'
 
-@project(name="fullstack")
+@project(name="foo_taxi_fare")
 @trigger(events=['s3'])
 @conda_base(libraries={'pandas': '1.4.2', 'pyarrow': '11.0.0', 'numpy': '1.21.2', 'scikit-learn': '1.1.2'})
 class TaxiFarePrediction(FlowSpec):
@@ -13,11 +16,20 @@ class TaxiFarePrediction(FlowSpec):
 
     def transform_features(self, df):
 
+        # TODO: 
+            # Try to complete tasks 2 and 3 with this function doing nothing like it currently is.
+            # Understand what is happening.
+            # Revisit task 1 and think about what might go in this function.
         obviously_bad_data_filters = [
 
             df.fare_amount > 0,         # fare_amount in US Dollars
             df.trip_distance <= 100,    # trip_distance in miles
-            df.trip_distance > 0
+            df.trip_distance > 0,
+            df.passenger_count > 0,
+            df.extra > 0,
+            df.mta_tax > 0,
+            df.tip_amount >= 0,
+            df.tolls_amount >= 0,
 
             # TODO: add some logic to filter out what you decide is bad data!
             # TIP: Don't spend too much time on this step for this project though, it practice it is a never-ending process.
@@ -26,7 +38,6 @@ class TaxiFarePrediction(FlowSpec):
 
         for f in obviously_bad_data_filters:
             df = df[f]
-            
         return df
 
     @step
